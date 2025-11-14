aa767eb7a262aaaa6bcbbed4472eae206a1970d8
Shiva Maleki <shiva.maleki@snapp.cab>
2025-11-14 15:34:00 +0330

    feat: setup project configuration and color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dm-element': 'hsl(209, 23%, 22%)',
+        'dm-bg': 'hsl(207, 26%, 17%)',
+        'lm-text': 'hsl(200, 15%, 8%)',
+        'lm-input': 'hsl(0, 0%, 52%)',
+        'lm-bg': 'hsl(0, 0%, 98%)',
+        'white': 'hsl(0, 0%, 100%)',
+      },
+      fontFamily: {
+        sans: ['Nunito Sans', 'sans-serif'],
+      }
+    },
   },
   plugins: [],
 }
-
