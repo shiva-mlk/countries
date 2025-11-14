a5603bb50ed2383c49c6721c99e6a1f9697dd05e
Shiva Maleki <shiva.maleki@snapp.cab>
2025-11-14 17:33:36 +0330

    feat: add search and filter components

---
+++ src/components/RegionFilter.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { REGIONS } from '@/constants/constant';
+
+defineProps<{
+  modelValue: string
+}>();
+
+defineEmits<{
+  (e: 'update:modelValue', value: string): void
+}>();
+</script>
+
+<template>
+  <div class="relative w-2/3 md:w-48 text-lm-text dark:text-white">
+    <select 
+      :value="modelValue"
+      @change="$emit('update:modelValue', ($event.target as HTMLSelectElement).value)"
+      class="appearance-none w-full p-4 pl-6 pr-12 rounded-lg shadow-md bg-white dark:bg-dm-element border-none focus:outline-none cursor-pointer transition-colors duration-300"
+    >
+      <option 
+        v-for="region in REGIONS" 
+        :key="region.value" 
+        :value="region.value"
+      >
+        {{ region.label }}
+      </option>
+    </select>
+    
+    <div class="absolute inset-y-0 right-0 flex items-center px-4 pointer-events-none">
+      <svg class="w-4 h-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+        <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/>
+      </svg>
+    </div>
+  </div>
+</template>
---
+++ src/components/SearchInput.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { MagnifyingGlassIcon } from '@heroicons/vue/24/outline';
+import { SEARCH_PLACEHOLDER } from '@/constants/constant';
+
+defineProps<{
+  modelValue: string
+}>();
+
+defineEmits<{
+  (e: 'update:modelValue', value: string): void
+}>();
+</script>
+
+<template>
+  <div class="relative w-full md:w-1/3 shadow-md rounded-lg text-lm-text dark:text-white">
+    <div class="absolute inset-y-0 left-0 pl-8 flex items-center pointer-events-none">
+      <MagnifyingGlassIcon class="h-5 w-5 text-lm-input dark:text-white" />
+    </div>
+    <input 
+      :value="modelValue"
+      @input="$emit('update:modelValue', ($event.target as HTMLInputElement).value)"
+      type="text"
+      :placeholder="SEARCH_PLACEHOLDER"
+      class="block w-full pl-20 p-4 rounded-lg bg-white dark:bg-dm-element placeholder-lm-input dark:placeholder-white focus:outline-none transition-colors duration-300"
+    />
+  </div>
+</template>
